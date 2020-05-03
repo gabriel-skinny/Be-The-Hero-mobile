e5c7818d5205b870335810ef248224602b2e2d34
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-05-03 14:09:17 -0300

    Changes

--- src/pages/incidents/index.js
+++ src/pages/incidents/index.js
@@ -57,7 +57,7 @@ export default function Incidents(){
 
             <Text style={styles.title}>Bem-vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo
-            e salve o dia!
+            e salve o dia!!
             </Text>
         
             <FlatList 
